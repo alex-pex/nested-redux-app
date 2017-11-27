9f0d39da856d2b938e26c43725cc4ff655e57012
alex-pex <alexandre.paixao@stadline.com>
2017-11-27 19:40:31 +0100

    Added redux with devtools

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../store/configureStore';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+class App extends Component {
+  constructor(...args) {
+    super(...args);
+    this.store = configureStore();
+  }
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Provider>
+    );
+  }
+}
 
 export default App;
