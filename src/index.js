517e51b84cc98e32a2449ccb8d970b23246bca87
alex-pex <alexandre.paixao@stadline.com>
2017-11-27 19:07:00 +0100

    Added strict eslint rules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import renderApp from './renderApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+renderApp();
 registerServiceWorker();
-
-if (module.hot) {
-  module.hot.accept("./components/App", () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
-  });
-}
