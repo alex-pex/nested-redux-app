2d72175b6cbde382896c796f8c0d7bcc77c4ee0e
alex-pex <alexandre.paixao@stadline.com>
2018-02-20 01:18:23 +0100

    Add redux devtools

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,14 @@
+import { connectReduxDevtools } from 'mst-middlewares';
+import remotedev from 'remotedev';
 import Store from './models/Store';
 
 const initialState = {
   counters: [{ value: 123 }, { value: 456 }]
 };
 
-export default () => Store.create(initialState);
+export default () => {
+  const store = Store.create(initialState);
+  connectReduxDevtools(remotedev, store);
+
+  return store;
+};
