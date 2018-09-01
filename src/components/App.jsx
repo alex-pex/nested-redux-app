1d0a4ccb52f5ddf3fa292fa955cf018e4d7bb986
alex-pex <alexandre.paixao@stadline.com>
2018-09-02 00:25:54 +0200

    Implement Increment and IncrementAsync actions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
+const App = ({ computing, value, increment, incrementAsync }) => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
@@ -11,7 +12,21 @@ const App = () => (
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
     </p>
+    <p>Status: {computing ? 'computing' : 'ready'}</p>
+    <button type="button" onClick={increment}>
+      Increment {value}
+    </button>
+    <button type="button" onClick={incrementAsync}>
+      Increment Async {value}
+    </button>
   </div>
 );
+
+App.propTypes = {
+  computing: PropTypes.bool.isRequired,
+  value: PropTypes.number.isRequired,
+  increment: PropTypes.func.isRequired,
+  incrementAsync: PropTypes.func.isRequired
+};
 
 export default App;
--- src/renderApp.jsx
+++ src/renderApp.jsx
@@ -3,12 +3,76 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-export default function renderApp() {
-  ReactDOM.render(<App />, document.getElementById('root'));
+let appState = {
+  value: 42,
+  pendingComputations: 0
+};
+
+const subscribers = [];
+const subscribe = subscriber => subscribers.push(subscriber);
+
+let handler;
+
+const dispatch = async ({ type, ...payload }) => {
+  console.log(type, ...payload);
+
+  let updates = [];
+  const setState = update => updates.push(update);
+  const collect = () => {
+    const collectedUpdates = updates;
+    updates = [];
+
+    if (collectedUpdates.length > 0) {
+      appState = collectedUpdates.reduce((nextState, update) => update(nextState), appState);
+      subscribers.map(subscriber => subscriber());
+      console.log('> render', appState);
+    }
+  };
+
+  const promise = handler(setState, { type, ...payload });
+  collect();
+
+  promise.then(collect);
+};
+
+const incrementAction = () => ({
+  type: 'INCREMENT'
+});
+
+const incrementAsyncAction = () => ({
+  type: 'INCREMENT_ASYNC'
+});
+
+handler = async (setState, action) => {
+  if (action.type === 'INCREMENT') {
+    setState(state => ({ ...state, value: state.value + 1 }));
+  } else if (action.type === 'INCREMENT_ASYNC') {
+    setState(state => ({ ...state, pendingComputations: state.pendingComputations + 1 }));
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    setState(state => ({ ...state, pendingComputations: state.pendingComputations - 1, value: state.value + 1 }));
+  }
+};
+
+function renderApp() {
+  ReactDOM.render(
+    <App
+      increment={() => dispatch(incrementAction())}
+      incrementAsync={() => dispatch(incrementAsyncAction())}
+      value={appState.value}
+      computing={appState.pendingComputations > 0}
+    />,
+    document.getElementById('root')
+  );
+}
+
+export default function() {
+  renderApp();
+
+  subscribe(renderApp);
 
   if (module.hot) {
     module.hot.accept('./components/App', () => {
-      ReactDOM.render(<App />, document.getElementById('root'));
+      renderApp();
     });
   }
 }
